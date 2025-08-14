e6e470314f305242c2599f98fd1144a03ff1b8ac
devsantoki211 <santokidev6@gmail.com>
2025-08-14 13:04:38 +0530

    Add files via upload

---
+++ CS188_1.CPP
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{/* program to add  nos.*/
+  int a,b,c;
+  clrscr();
+  printf("Enter 2 numbers:");
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("%d + %d = %d\n",a,b,c);
+  getch();
+}
